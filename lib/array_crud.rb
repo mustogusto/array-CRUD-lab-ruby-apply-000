a5d3456b2452e5c9b6d71225c4ee93c20afff3aa
Aisha S <a_shaheed@outlook.com>
2020-04-03 06:01:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ def create_an_array
 ['a', 'v', 'e', 'd']
 end
 
-def add_element_to_end_of_array(array = ['p', 'e', 'a'], element = 's')
+def add_element_to_end_of_array(array, element = 's')
+array = ['p', 'e', 'a']
 array << 's'
 end
 
